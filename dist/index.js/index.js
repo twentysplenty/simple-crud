d2f3a4dd7c93de52e7e3d85dae6032ff0d0d4774
Pelagey Sergey <someadress@somemail.com>
2025-05-12 00:07:56 +0000

    refactor: change scripts

--- dist/index.js/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv/config");
-var server_1 = require("./src/server");
-var PORT = process.env.PORT;
-(0, server_1.startSever)(PORT);
-//# sourceMappingURL=index.js.map
